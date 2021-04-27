6953df0b7c821e93d3c3d60bbc5eed061fd10394
Ihor Barmak <ihor@uplab.io>
2021-04-27 20:05:12 +0300

    added stateless login form

--- src/components/layouts/AuthLayout.js
+++ src/components/layouts/AuthLayout.js
@@ -1,7 +1,6 @@
-import { Nav, Navbar, Container, Button } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import paths from '../../router/paths';
-import AuthManager from '../../services/AuthManager';
 
 export default function AuthLayout({ children }) {
   return (
@@ -13,15 +12,12 @@ export default function AuthLayout({ children }) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            {/* <Nav.Link as={Link} to={paths.login}>
+            <Nav.Link as={Link} to={paths.login}>
               Login
             </Nav.Link>
-            <Nav.Link as={Link} to={paths.signUp}>
+            {/* <Nav.Link as={Link} to={paths.signUp}>
               Sign up
             </Nav.Link> */}
-            <Button onClick={() => AuthManager.login('hardcoded_token')}>
-              Login
-            </Button>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,77 @@
+import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Button, Form } from 'react-bootstrap';
+import AuthManager from '../services/AuthManager';
 import paths from '../router/paths';
 
-export default function Login() {
-  return (
-    <Row className="mt-5">
-      <Col
-        lg={{ span: 6, offset: 3 }}
-        md={{ span: 8, offset: 2 }}
-        sm={{ span: 12 }}
-      >
-        <Card>
-          <Card.Body>
-            <Link to={paths.signUp} className="float-right">
-              <Button type="button" variant="outline-primary">
-                Sign up
-              </Button>
-            </Link>
-            <Card.Title>Sign in</Card.Title>
-            <Form>
-              <Form.Group>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-              <Form.Group>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-            </Form>
-            <Button variant="primary" type="submit" block>
-              Submit
-            </Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  );
+  onChangeEmail = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  onChangePassword = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  onSubmit = () => {
+    const { email } = this.state;
+    console.log(`Logging in as email ${email}`);
+    AuthManager.login();
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <Row className="mt-5">
+        <Col
+          lg={{ span: 6, offset: 3 }}
+          md={{ span: 8, offset: 2 }}
+          sm={{ span: 12 }}
+        >
+          <Card>
+            <Card.Body>
+              <Link to={paths.signUp} className="float-right">
+                <Button type="button" variant="outline-primary">
+                  Sign up
+                </Button>
+              </Link>
+              <Card.Title>Sign in</Card.Title>
+              <Form onSubmit={this.onSubmit}>
+                <Form.Group>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    value={email}
+                    onChange={this.onChangeEmail}
+                    type="email"
+                    placeholder="Enter email"
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    value={password}
+                    onChange={this.onChangePassword}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" block>
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
 }
