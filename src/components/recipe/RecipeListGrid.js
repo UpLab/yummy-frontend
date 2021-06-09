2ed6ecc5837c556e1f92f006343fdaa7d3f2ad6c
Ihor Barmak <ihor@uplab.io>
2021-06-09 17:59:25 +0300

    refactor: RecipeListGrid uses hooks

--- src/components/recipe/RecipeListGrid.js
+++ src/components/recipe/RecipeListGrid.js
@@ -1,19 +1,15 @@
-/* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import RecipeListCard from './RecipeListCard';
 
-export default class RecipeListGrid extends Component {
-  render() {
-    const { recipeList } = this.props;
-    return (
-      <Row>
-        {recipeList.map((recipe) => (
-          <Col key={recipe._id} lg="3" md="4" sm="6" xs="6" className="mb-4">
-            <RecipeListCard recipe={recipe} />
-          </Col>
-        ))}
-      </Row>
-    );
-  }
+export default function RecipeListGrid(props) {
+  const { recipeList } = props;
+  return (
+    <Row>
+      {recipeList.map((recipe) => (
+        <Col key={recipe._id} lg="3" md="4" sm="6" xs="6" className="mb-4">
+          <RecipeListCard recipe={recipe} />
+        </Col>
+      ))}
+    </Row>
+  );
 }
