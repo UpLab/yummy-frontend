770c2e8dd129ab9f681a5557f2a35835212dee90
Ihor Barmak <ihor@uplab.io>
2021-07-10 15:11:40 +0300

    recipe details

--- src/components/common/PageTitle.js
+++ src/components/common/PageTitle.js
@@ -1,6 +1,6 @@
 export default function PageTitle({ title, right }) {
   return (
-    <div className="d-flex align-items-center justify-content-between mt-3">
+    <div className="d-flex align-items-center justify-content-between mt-3 mb-3">
       <h1>{title}</h1>
       {right}
     </div>
--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,14 +1,28 @@
 import { generatePath, Link, useHistory, useParams } from 'react-router-dom';
-import { Alert } from 'react-bootstrap';
+import {
+  Alert,
+  Breadcrumb,
+  ButtonGroup,
+  Card,
+  Col,
+  Image,
+  Row,
+} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {
+  faClock,
+  faPencilAlt,
+  faTrash,
+  faUtensils,
+} from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
+import { format } from 'date-fns';
 import Button from '../components/common/Button';
 import routePaths from '../router/paths';
-import RecipeListCard from '../components/recipe/RecipeListCard';
 import useAPIQuery from '../hooks/useAPIQuery';
 import APIService from '../services/APIService';
 import useAPIMethod from '../hooks/useAPIMethod';
+import PageTitle from '../components/common/PageTitle';
 
 export default function RecipeDetails() {
   const params = useParams();
@@ -41,34 +55,106 @@ export default function RecipeDetails() {
     return <Alert variant="danger">Recipe not found</Alert>;
   }
   return (
-    <p>
-      <h1>
-        {recipe.name}{' '}
-        <Link to={generatePath(routePaths.editRecipe, { id: recipe._id })}>
-          <Button size="sm" variant="outline-primary">
-            <FontAwesomeIcon icon={faPencilAlt} />
-          </Button>
-        </Link>
-        <Button
-          size="sm"
-          variant="outline-danger"
-          onClick={() => {
-            // eslint-disable-next-line no-restricted-globals
-            const confirmed = confirm(
-              'Are you sure you want to delete this recipe?'
-            );
-            if (confirmed) {
-              deleteRecipe(recipe._id);
-            }
-          }}
-          loading={isDeletingRecipe}
-        >
-          <FontAwesomeIcon icon={faTrash} />
-        </Button>
-      </h1>
-
-      <RecipeListCard recipe={recipe} />
-      <pre>{JSON.stringify(recipe, null, 2)}</pre>
-    </p>
+    <>
+      <Breadcrumb className="mt-3">
+        <Breadcrumb.Item linkAs={Link} linkProps={{ to: routePaths.myRecipes }}>
+          My Recipes
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>{recipe.name}</Breadcrumb.Item>
+      </Breadcrumb>
+      <PageTitle
+        title={recipe.name}
+        right={
+          <ButtonGroup>
+            <Button
+              size="sm"
+              variant="outline-primary"
+              as={Link}
+              to={generatePath(routePaths.editRecipe, { id: recipe._id })}
+            >
+              <FontAwesomeIcon icon={faPencilAlt} />
+            </Button>
+            <Button
+              size="sm"
+              variant="outline-danger"
+              onClick={() => {
+                // eslint-disable-next-line no-restricted-globals
+                const confirmed = confirm(
+                  'Are you sure you want to delete this recipe?'
+                );
+                if (confirmed) {
+                  deleteRecipe(recipe._id);
+                }
+              }}
+              loading={isDeletingRecipe}
+            >
+              <FontAwesomeIcon icon={faTrash} />
+            </Button>
+          </ButtonGroup>
+        }
+      />
+      <div>
+        <Row>
+          <Col md={7}>
+            <Image
+              src={recipe.featuredImage}
+              alt={recipe.name}
+              rounded
+              style={{ width: '100%', maxHeight: 400, objectFit: 'cover' }}
+            />
+          </Col>
+          <Col>
+            <Card className="mb-3">
+              <Card.Body>
+                <Card.Title>Info</Card.Title>
+                <FontAwesomeIcon icon={faClock} /> {recipe.cookTimeMinutes} min
+                <br />
+                <FontAwesomeIcon icon={faUtensils} /> {recipe.servings}
+                <br />
+                <small className="text-muted float-right">
+                  Last update:{' '}
+                  {format(
+                    new Date(recipe.updatedAt || recipe.createdAt),
+                    'dd MMM yyyy'
+                  )}
+                </small>
+              </Card.Body>
+            </Card>
+            {Array.isArray(recipe.ingredients) && recipe.ingredients.length ? (
+              <Card>
+                <Card.Body>
+                  <Card.Title>Ingredients</Card.Title>
+                  <ul>
+                    {recipe.ingredients.map((ingredient) => (
+                      <li>
+                        {ingredient.value} {ingredient.unit} –{' '}
+                        {ingredient.title}
+                      </li>
+                    ))}
+                  </ul>
+                </Card.Body>
+              </Card>
+            ) : null}
+          </Col>
+        </Row>
+        <div className="mt-3">
+          <h2>Instructions</h2>
+          <p className="mt-3">
+            <ol>
+              {recipe.instructions.map((step, i) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <li key={i}>{step}</li>
+              ))}
+            </ol>
+          </p>
+          {recipe.notes ? (
+            <Alert variant="info">
+              <b>Note: </b>
+              {recipe.notes}
+            </Alert>
+          ) : null}
+        </div>
+      </div>
+    </>
   );
 }
